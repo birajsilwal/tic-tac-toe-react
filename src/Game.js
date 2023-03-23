8dbc8bc369da6dcfba96c34ed7888bc1e6bd37f5
Biraj Silwal <birajsilwal.ai@gmail.com>
2023-03-23 00:36:46 -0500

    decide the winner

--- src/Game.js
+++ src/Game.js
@@ -1,30 +1,61 @@
 import React, { useState } from 'react';
 import Board from './Board';
 
-const Game = () => {
+function Game() {
   const [squares, setSquares] = useState(Array(9).fill(null));
-  const [player, setPlayer] = useState('X');
+  const [xIsNext, setXIsNext] = useState(true);
 
-  const handleClick = (i) => {
-    const squaresCopy = [...squares];
-    squaresCopy[i] = player;
+  function handleClick(i) {
+    const squaresCopy = squares.slice();
+
+    if (calculateWinner(squaresCopy) || squaresCopy[i]) {
+      // game is over or square is already filled
+      return;
+    }
+
+    squaresCopy[i] = xIsNext ? 'X' : 'O';
     setSquares(squaresCopy);
-    setPlayer(player === 'X' ? 'O' : 'X');
-  };
+    setXIsNext(!xIsNext);
+  }
 
-  const status = `Next player: ${player}`;
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board squares={squares} onClick={handleClick} />
+        <Board squares={squares} onClick={(i) => handleClick(i)} />
       </div>
       <div className="game-info">
         <div>{status}</div>
-        <ol>{/* TODO */}</ol>
       </div>
     </div>
   );
-};
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 export default Game;
