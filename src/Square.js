c32e92c8ccdb65e41dcffc90c1260313c29e47df
Biraj Silwal <birajsilwal.ai@gmail.com>
2023-03-23 00:46:28 -0500

    increase the size of the box but all in the same line

--- src/Square.js
+++ src/Square.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
-const Square = ({ value, onClick }) => {
+function Square(props) {
+  const squareStyle = {
+    fontSize: '24px',
+    width: '48px',
+    height: '48px',
+    background: '#fff',
+    border: '1px solid #999',
+    float: 'left',
+    marginRight: '-1px',
+    marginTop: '-1px',
+    padding: '0',
+    textAlign: 'center',
+    lineHeight: '48px',
+    outline: 'none',
+    cursor: 'pointer'
+  };
+
   return (
-    <button className="square" onClick={onClick}>
-      {value}
+    <button style={squareStyle} className="square" onClick={props.onClick}>
+      {props.value}
     </button>
   );
-};
+}
 
 export default Square;
